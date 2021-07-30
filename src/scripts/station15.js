5e1439868a8f65cda52afd0f1200ea617114d0f7
Takus0731 <ba.takusea@icloud.com>
2021-07-30 16:46:26 +0900

    test

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,24 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await userList.map(test);
+  //const result = await userList.map(test);
+  const result = await test(userList );
   return await result;
 }
+function test(userList){
+  promise = new Promise((resolve) =>{
 
-function test(data) {
+    setTimeout(() => {
+      const result = userList.map((v)=>({
+        full_name: v.family_name + " " + v.first_name,
+      }));
+      resolve(result);
+    }, 3000);
+  });
+  return promise;
+
+}
+/*function test(data) {
   new Promise((resolve) => {
     setTimeout(() => {
       resolve(data);
@@ -18,5 +31,5 @@ function test(data) {
   })
     data.full_name = data.family_name + " " + data.first_name;
     return data;
-};
+};*/
 
